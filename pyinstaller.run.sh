a94ac3605de65fba011342e7a0d7bbf514436c36
piotrj <piotrj@alatek.krakow.pl>
2022-11-26 21:02:57 +0100

    CI windows + linux

--- pyinstaller.run.sh
+++ pyinstaller.run.sh
@@ -1,2 +1,2 @@
 echo running-pyinstaller
-python -m PyInstaller --noconsole --onefile --clean  --add-data="LICENSE:." --add-data="keyboard.shortcuts.txt:." --add-data="icon.png:." --icon=icon.ico ./dude.py
+pyinstaller --noconsole --onefile --clean  --add-data="LICENSE:." --add-data="keyboard.shortcuts.txt:." --add-data="icon.png:." --icon=icon.ico ./dude.py
