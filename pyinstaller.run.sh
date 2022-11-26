86f2b9087b9eb9655418aeedc6cbc2a2e06896d8
piotrj <piotrj@alatek.krakow.pl>
2022-11-26 12:42:30 +0100

    run by pyhon

--- pyinstaller.run.sh
+++ pyinstaller.run.sh
@@ -1,1 +1,2 @@
-pyinstaller --noconsole --onefile --clean  --add-data="LICENSE:." --add-data="keyboard.shortcuts.txt:." --add-data="icon.png:." --icon=icon.ico ./dude.py
+echo running-pyinstaller
+python -m PyInstaller --noconsole --onefile --clean  --add-data="LICENSE:." --add-data="keyboard.shortcuts.txt:." --add-data="icon.png:." --icon=icon.ico ./dude.py
