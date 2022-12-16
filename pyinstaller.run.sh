e6d8d1eb156822f7db2b8771337c0f534be44c94
piotrj <piotrj@alatek.krakow.pl>
2022-12-16 21:17:40 +0100

    fix CI

--- pyinstaller.run.sh
+++ pyinstaller.run.sh
@@ -1,2 +1,3 @@
 echo running-pyinstaller
+mkdir ./dist
 pyinstaller --noconsole --onefile --clean  --add-data="LICENSE:." --add-data="keyboard.shortcuts.txt:." --add-data="icon.png:." --icon=icon.ico ./dude.py 2>&1 | tee ./dist/pyinstaller.lin.log
