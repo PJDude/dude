e4b09f3c3f45f7a7eb327a7b0fc84fe6b50b6685
piotrj <piotrj@alatek.krakow.pl>
2022-12-16 20:49:25 +0100

    toolset update

--- pyinstaller.run.bat
+++ pyinstaller.run.bat
@@ -1,3 +1,3 @@
-rem PATH=%PATH%;c:\Users\%USERNAME%\AppData\Roaming\Python\Python310\Scripts
+@rem PATH=%PATH%;c:\Users\%USERNAME%\AppData\Roaming\Python\Python310\Scripts
 
-pyinstaller --noconsole --onefile --clean --add-data="LICENSE;." --add-data="keyboard.shortcuts.txt;."  --add-data="icon.png;." --icon=icon.ico dude.py
+pyinstaller --noconsole --onefile --clean --add-data="LICENSE;." --add-data="keyboard.shortcuts.txt;."  --add-data="icon.png;." --icon=icon.ico dude.py | tee dist\pyinstaller.win.log
--- pyinstaller.run.sh
+++ pyinstaller.run.sh
@@ -1,2 +1,2 @@
 echo running-pyinstaller
-pyinstaller --noconsole --onefile --clean  --add-data="LICENSE:." --add-data="keyboard.shortcuts.txt:." --add-data="icon.png:." --icon=icon.ico ./dude.py
+pyinstaller --noconsole --onefile --clean  --add-data="LICENSE:." --add-data="keyboard.shortcuts.txt:." --add-data="icon.png:." --icon=icon.ico ./dude.py 2>&1 | tee ./dist/pyinstaller.lin.log
