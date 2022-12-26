a3ddf5eed3d8864440553ee5d5961d328827b082
piotrj <piotrj@alatek.krakow.pl>
2022-12-26 18:22:54 +0100

    crazy tests generator [skip ci]

--- test.py
+++ test.py
@@ -2,26 +2,35 @@
 
 import os
 import pathlib
-from random import randint
-from random import choice
+import random
 import string
 
-def generate(Dir, MaxSubDirsQuant=4, MaxFilesQuant=64,NextLevels=6,MaxSize=65536):
+def generate(Dir, MaxSubDirsQuant=4, MaxFilesQuant=128,NextLevels=7,MaxSize=65536):
     print(f'generating test dir:{Dir} ...')
+    random.seed(111)
     generateCore(Dir, MaxSubDirsQuant, MaxFilesQuant,NextLevels,MaxSize)
     print('Done.')
     
+Traps = ' \'"{}[]`!@#$%^&,.()' if os.name=='nt' else ' \'"{}[]`!@#$%^&*,.<>():;'
+#Traps = string.ascii_letters
+
+def TrapChar():
+    return random.choice(Traps)
+    
 def generateCore(Dir, MaxSubDirsQuant, MaxFilesQuant,NextLevels,MaxSize):
     pathlib.Path(Dir).mkdir(parents=True,exist_ok=True)
-    
-    for i in range(randint(1,MaxFilesQuant)):
-        with open(os.sep.join((Dir,'file' + str(i))) + '.txt', "w") as f:
-            f.seek(randint(1,MaxSize))
-            f.write(choice(string.ascii_letters))
-    
+
+    for i in range(random.randint(1,MaxFilesQuant)):
+        try:
+            with open(os.sep.join((Dir,TrapChar() + TrapChar() + TrapChar())) + '.txt', "w") as f:
+                f.seek(random.randint(1,MaxSize))
+                f.write(random.choice(string.ascii_uppercase))
+        except:
+            pass
+            
     if NextLevels:
-        for i in range(randint(1,MaxSubDirsQuant)):
-            generateCore(os.sep.join((Dir,choice(string.ascii_letters))), MaxSubDirsQuant, MaxFilesQuant,NextLevels-1,MaxSize)
+        for i in range(random.randint(1,MaxSubDirsQuant)):
+            generateCore(os.sep.join((Dir,TrapChar() + TrapChar() + TrapChar())), MaxSubDirsQuant, MaxFilesQuant,NextLevels-1,MaxSize)
 
 if __name__ == "__main__":
     generate('testdir')
