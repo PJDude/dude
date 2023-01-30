d72fba6aa99ca79132c429e60cc1b5ca7fd2ae70
piotrj <piotrj@alatek.krakow.pl>
2023-01-30 20:10:27 +0100

    fix flow, noconsole on windows

--- nuitka.run.bat
+++ nuitka.run.bat
@@ -1,2 +1,2 @@
 @rem PATH=%PATH%;c:\Users\%USERNAME%\AppData\Roaming\Python\Python310\Scripts
-python -m nuitka --follow-imports --onefile --assume-yes-for-downloads --include-data-files=./icon.png=./icon.png --include-data-files=./LICENSE=./LICENSE --include-data-files=./keyboard.shortcuts.txt=./keyboard.shortcuts.txt --enable-plugin=tk-inter ./dude.py
+python -m nuitka --follow-imports --onefile --disable-console --assume-yes-for-downloads --include-data-files=./icon.png=./icon.png --include-data-files=./LICENSE=./LICENSE --include-data-files=./keyboard.shortcuts.txt=./keyboard.shortcuts.txt --enable-plugin=tk-inter ./dude.py
