fb404661ba3d38faf8e73ab8f7fa1cdda54bd841
piotrj <piotrj@alatek.krakow.pl>
2023-02-12 14:39:25 +0100

    fix(?) artifact upload

--- nuitka.run.sh
+++ nuitka.run.sh
@@ -1,3 +1,1 @@
-echo running-nuitka
-
 python3.10 -m nuitka --follow-imports --follow-stdlib --onefile --include-data-files=./icon.png=./icon.png --include-data-files=./LICENSE=./LICENSE --include-data-files=./keyboard.shortcuts.txt=./keyboard.shortcuts.txt --enable-plugin=tk-inter -output-filename=dude --remove-output ./dude.py
