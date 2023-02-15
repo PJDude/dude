6aecf26a13e6c728050bffb803cd19f540af8723
piotrj <piotrj@alatek.krakow.pl>
2023-02-15 19:18:02 +0100

    Nuitka options

--- nuitka.run.bat
+++ nuitka.run.bat
@@ -1,6 +1,6 @@
 @echo building main app
-python -m nuitka --follow-imports --follow-stdlib --onefile --disable-console --assume-yes-for-downloads --force-stdout-spec=dude.stdout.log --force-stderr-spec=dude.stderr.log --windows-icon-from-ico=./icon.ico --include-data-files=./icon.png=./icon.png --include-data-files=./LICENSE=./LICENSE --include-data-files=./keyboard.shortcuts.txt=./keyboard.shortcuts.txt --enable-plugin=tk-inter --remove-output --output-filename=dudegui.exe ./dude.py
+python -m nuitka --follow-imports --follow-stdlib --onefile --show-scons --show-progress --show-modules --disable-console --assume-yes-for-downloads --force-stdout-spec=dude.stdout.log --force-stderr-spec=dude.stderr.log --windows-icon-from-ico=./icon.ico --include-data-files=./icon.png=./icon.png --include-data-files=./LICENSE=./LICENSE --include-data-files=./keyboard.shortcuts.txt=./keyboard.shortcuts.txt --enable-plugin=tk-inter --remove-output --output-filename=dudegui.exe --lto=yes ./dude.py
 
 @echo building console wrapper
 @rem --include-data-files=dudeg.exe=dudegui.exe
-python -m nuitka --follow-imports --follow-stdlib --onefile --assume-yes-for-downloads --windows-icon-from-ico=./icon.ico --remove-output --output-filename=dude.exe ./console.py
+python -m nuitka --follow-imports --follow-stdlib --onefile --show-scons --show-progress --show-modules --assume-yes-for-downloads --windows-icon-from-ico=./icon.ico --remove-output --output-filename=dude.exe --lto=yes ./console.py
