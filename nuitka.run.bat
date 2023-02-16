d1e2bd21d9d770ae270f8f25c60975ef2e8014dc
piotrj <piotrj@alatek.krakow.pl>
2023-02-16 18:30:03 +0100

    keyboard.shortcuts.txt removed (everything on the menus)

--- nuitka.run.bat
+++ nuitka.run.bat
@@ -1,5 +1,5 @@
 @echo building main app
-python -m nuitka --follow-imports --follow-stdlib --onefile --show-scons --show-progress --show-modules --disable-console --assume-yes-for-downloads --force-stdout-spec=dude.stdout.log --force-stderr-spec=dude.stderr.log --windows-icon-from-ico=./icon.ico --include-data-files=./icon.png=./icon.png --include-data-files=./LICENSE=./LICENSE --include-data-files=./keyboard.shortcuts.txt=./keyboard.shortcuts.txt --enable-plugin=tk-inter --remove-output --output-filename=dudegui.exe --lto=yes ./dude.py
+python -m nuitka --follow-imports --follow-stdlib --onefile --show-scons --show-progress --show-modules --disable-console --assume-yes-for-downloads --force-stdout-spec=dude.stdout.log --force-stderr-spec=dude.stderr.log --windows-icon-from-ico=./icon.ico --include-data-files=./icon.png=./icon.png --include-data-files=./LICENSE=./LICENSE --enable-plugin=tk-inter --remove-output --output-filename=dudegui.exe --lto=yes ./dude.py
 
 @echo building console wrapper
 @rem --include-data-files=dudeg.exe=dudegui.exe
