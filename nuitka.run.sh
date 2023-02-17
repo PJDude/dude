b1ad439b3dd7ed70e98a090459d2f5ee8ba9efdf
piotrj <piotrj@alatek.krakow.pl>
2023-02-17 18:56:53 +0100

    LDFLAGS

--- nuitka.run.sh
+++ nuitka.run.sh
@@ -1,1 +1,2 @@
-CCFLAGS='-Ofast -static' LDFLAGS='-s -static-libgcc -flinker-output=exec' python3.10 -m nuitka --follow-imports --follow-stdlib --onefile --linux-icon=./icon.ico --show-scons --show-progress --show-modules --include-data-files=./icon.png=./icon.png --include-data-files=./LICENSE=./LICENSE --enable-plugin=tk-inter --output-filename=dude --lto=yes ./dude.py 2>&1 | tee ./nuitka.log
+CCFLAGS='-Ofast -static' LDFLAGS='-flinker-output=exec' python3.10 -m nuitka --follow-imports --follow-stdlib --onefile --linux-icon=./icon.ico --show-scons --show-progress --show-modules --include-data-files=./icon.png=./icon.png --include-data-files=./LICENSE=./LICENSE --enable-plugin=tk-inter --output-filename=dude --lto=yes ./dude.py 2>&1 | tee ./nuitka.log
+#-s -static-libgcc
