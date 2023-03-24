7581d36aa56a7e21a15e84b71b2cefdbbc857eff
piotrj <piotr.jochymek@gmail.com>
2023-03-24 21:21:49 +0100

    experimental non-onefile win distro

--- scripts/nuitka.run.bat
+++ scripts/nuitka.run.bat
@@ -1,6 +1,7 @@
 @cd "%~dp0..\src"
 
-@echo building main app
-python -m nuitka --follow-imports --follow-stdlib --onefile --show-scons --show-progress --show-modules --assume-yes-for-downloads --windows-icon-from-ico=./icon.ico --include-data-file=./version.txt=./version.txt --include-data-file=./icon.png=./icon.png --include-data-file=./../LICENSE=./LICENSE --enable-plugin=tk-inter --remove-output --output-filename=dude.exe --lto=yes --remove-output ./dude.py
+@echo building main app raw
+python -m nuitka --follow-imports --follow-stdlib --onefile --show-scons --show-progress --show-modules --assume-yes-for-downloads --windows-icon-from-ico=./icon.ico --include-data-file=./version.txt=./version.txt --include-data-file=./icon.png=./icon.png --include-data-file=./../LICENSE=./LICENSE --enable-plugin=tk-inter --output-filename=dude.exe --output-dir=.. --lto=yes ./dude.py
 
 move dude.exe ../dude.exe
+move ../dude.dist ../dude
