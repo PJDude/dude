a494767e1756cd5f5c8b21ba6f305c00075050dc
piotrj <piotr.jochymek@gmail.com>
2023-04-19 20:29:03 +0200

    python version fix

--- scripts/nuitka.run.sh
+++ scripts/nuitka.run.sh
@@ -11,7 +11,7 @@ rm -rf ../build-nuitka-lin
 mkdir ../build-nuitka-lin
 
 echo running-nuitka
-CCFLAGS='-Ofast -static' python3.10 -m nuitka --follow-imports --follow-stdlib --onefile --linux-icon=./icon.ico --show-scons --show-progress --show-modules --include-data-file=./version.txt=./version.txt --include-data-file=./../LICENSE=./LICENSE --enable-plugin=tk-inter --output-filename=../build-nuitka-lin/dude --output-dir=../build-nuitka-lin --lto=yes ./dude.py
+CCFLAGS='-Ofast -static' python3.11 -m nuitka --follow-imports --follow-stdlib --onefile --linux-icon=./icon.ico --show-scons --show-progress --show-modules --include-data-file=./version.txt=./version.txt --include-data-file=./../LICENSE=./LICENSE --enable-plugin=tk-inter --output-filename=../build-nuitka-lin/dude --output-dir=../build-nuitka-lin --lto=yes ./dude.py
 # --file-description='DUplicates DEtector'
 # --copyright='2022-2023 Piotr Jochymek'
 #--product-version=$VERSION
