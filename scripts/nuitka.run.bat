506e054eaaaf80cbc4bbbd1af22510fd745b455b
piotrj <piotr.jochymek@gmail.com>
2023-09-14 16:01:13 +0200

    abort on error, reduce nuitka log level , python 3.11 on linux

--- scripts/nuitka.run.bat
+++ scripts/nuitka.run.bat
@@ -12,30 +12,27 @@ rmdir /s /q %OUTDIR%
 SET OUTDIR_C=%OUTDIR%\C
 SET OUTDIR_G=%OUTDIR%\G
 
-rem SET OUTDIR_O_C=%OUTDIR%\O_C
-rem SET OUTDIR_O_G=%OUTDIR%\O_G
-
 mkdir %OUTDIR%
 
 mkdir %OUTDIR_C%
 mkdir %OUTDIR_G%
 
-rem mkdir %OUTDIR_O_C%
-rem mkdir %OUTDIR_O_G%
+python -m nuitka --version > distro.info.txt || exit /b 1
 
-python -m nuitka --version > distro.info.txt
+@rem --show-scons --show-progress --show-modules
 
-python -m nuitka --file-reference-choice=runtime --follow-imports --follow-stdlib --onefile --show-scons --show-progress --show-modules --assume-yes-for-downloads --windows-icon-from-ico=./icon.ico --include-data-file=./distro.info.txt=./distro.info.txt --include-data-file=./version.txt=./version.txt --include-data-file=./../LICENSE=./LICENSE --output-filename=dudecmd.exe --output-dir=%OUTDIR_C% --lto=yes --product-name=dudecmd --product-version=%VERSION% --copyright="2022-2023 Piotr Jochymek" --file-description="DUplicates DEtector" ./console.py
+@rem --file-reference-choice=runtime
+@rem Nuitka-Options:INFO: Using default file reference mode 'runtime' need not be specified.
 
-python -m nuitka --file-reference-choice=runtime --follow-imports --onefile --show-scons --show-progress --show-modules --assume-yes-for-downloads --windows-icon-from-ico=./icon.ico --include-data-file=./distro.info.txt=./distro.info.txt --include-data-file=./version.txt=./version.txt --include-data-file=./../LICENSE=./LICENSE --enable-plugin=tk-inter --output-filename=dude.exe --output-dir=%OUTDIR_G% --lto=yes --product-name=dude --product-version=%VERSION% --copyright="2022-2023 Piotr Jochymek" --file-description="DUplicates DEtector" --disable-console ./dude.py
+@rem --follow-imports
+@rem Nuitka-Options:INFO: Following all imports is the default for onefile mode and need not be specified.
+
+python -m nuitka --follow-stdlib --onefile --assume-yes-for-downloads --windows-icon-from-ico=./icon.ico --include-data-file=./distro.info.txt=./distro.info.txt --include-data-file=./version.txt=./version.txt --include-data-file=./../LICENSE=./LICENSE --output-filename=dudecmd.exe --output-dir=%OUTDIR_C% --lto=yes --product-name=dudecmd --product-version=%VERSION% --copyright="2022-2023 Piotr Jochymek" --file-description="DUplicates DEtector" ./console.py || exit /b 2
+
+python -m nuitka --onefile --assume-yes-for-downloads --windows-icon-from-ico=./icon.ico --include-data-file=./distro.info.txt=./distro.info.txt --include-data-file=./version.txt=./version.txt --include-data-file=./../LICENSE=./LICENSE --enable-plugin=tk-inter --output-filename=dude.exe --output-dir=%OUTDIR_G% --lto=yes --product-name=dude --product-version=%VERSION% --copyright="2022-2023 Piotr Jochymek" --file-description="DUplicates DEtector" --disable-console ./dude.py || exit /b 3
 
 move %OUTDIR_C%\console.dist\dudecmd.exe %OUTDIR_G%\dude.dist
 move %OUTDIR_G%\dude.dist %OUTDIR_G%\dude
-
-rem uploadowane pojedynczo
-rem powershell Compress-Archive -Path %OUTDIR_G%\dudecmd.exe,%OUTDIR_G%\dude.exe -DestinationPath %OUTDIR%\dude.nuitka.win.onefile.zip
-REM move %OUTDIR_C%\dudecmd.exe %OUTDIR_G%
-
 
 del %OUTDIR%\dude.nuitka.win.zip
 powershell Compress-Archive %OUTDIR_G%\dude %OUTDIR%\dude.nuitka.win.zip
--- scripts/pyinstaller.run.bat
+++ scripts/pyinstaller.run.bat
@@ -16,19 +16,15 @@ SET OUTDIR_G=%OUTDIR%\G
 SET OUTDIR_O_C=%OUTDIR%\O_C
 SET OUTDIR_O_G=%OUTDIR%\O_G
 
-pyinstaller --clean --add-data="version.txt;." --add-data="../LICENSE;." --icon=icon.ico --distpath=%OUTDIR_C% --console --name dudecmd console.py
-pyinstaller --clean --add-data="version.txt;." --add-data="../LICENSE;." --icon=icon.ico --distpath=%OUTDIR_G% --windowed --name dude dude.py
-pyinstaller --clean --add-data="version.txt;." --add-data="../LICENSE;." --icon=icon.ico --distpath=%OUTDIR_O_C% --console --name dudecmd --onefile console.py
-pyinstaller --clean --add-data="version.txt;." --add-data="../LICENSE;." --icon=icon.ico --distpath=%OUTDIR_O_G% --windowed --name dude --onefile dude.py
+pyinstaller --clean --add-data="version.txt;." --add-data="../LICENSE;." --icon=icon.ico --distpath=%OUTDIR_C% --console --name dudecmd console.py  || exit /b 1
+pyinstaller --clean --add-data="version.txt;." --add-data="../LICENSE;." --icon=icon.ico --distpath=%OUTDIR_G% --windowed --name dude dude.py  || exit /b 2
+pyinstaller --clean --add-data="version.txt;." --add-data="../LICENSE;." --icon=icon.ico --distpath=%OUTDIR_O_C% --console --name dudecmd --onefile console.py  || exit /b 3
+pyinstaller --clean --add-data="version.txt;." --add-data="../LICENSE;." --icon=icon.ico --distpath=%OUTDIR_O_G% --windowed --name dude --onefile dude.py  || exit /b 4
 
 move %OUTDIR_C%\dudecmd\dudecmd.exe %OUTDIR_G%\dude
 
 del %OUTDIR%\dude.pyinstaller.win.zip
 powershell Compress-Archive %OUTDIR_G%\dude %OUTDIR%\dude.pyinstaller.win.zip
-
-rem uploadowane pojedynczo
-rem move %OUTDIR_O_C%\dudecmd.exe %OUTDIR_O_G%
-rem powershell Compress-Archive %OUTDIR_O_G%\*.exe ..\dude.pyinstaller.win.onefile.zip
 
 exit
 
