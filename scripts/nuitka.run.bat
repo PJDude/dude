8620236d687b63148eb2f43e78225e9b0ae8a245
piotrj <piotr.jochymek@gmail.com>
2024-01-27 09:18:26 +0100

    dudecmd fix, errors handling

--- scripts/nuitka.run.bat
+++ scripts/nuitka.run.bat
@@ -21,11 +21,11 @@ python -m nuitka --version >> distro.info.txt
 
 @echo.
 @echo running-nuitka-stage_dude
-python -m nuitka --include-data-file=./distro.info.txt=./distro.info.txt --include-data-file=./version.txt=./version.txt --include-data-file=../LICENSE=./LICENSE --enable-plugin=tk-inter --lto=yes --follow-stdlib  --assume-yes-for-downloads --windows-disable-console --output-dir=%outdir% --standalone --output-filename=dude ./dude.py
+python -m nuitka --include-data-file=./distro.info.txt=./distro.info.txt --include-data-file=./version.txt=./version.txt --include-data-file=../LICENSE=./LICENSE --enable-plugin=tk-inter --lto=yes --follow-stdlib  --assume-yes-for-downloads --windows-disable-console --output-dir=%outdir% --standalone --output-filename=dude ./dude.py || exit /b 2
 
 @echo.
 @echo running-nuitka-stage_dudecmd
-python -m nuitka --include-data-file=./distro.info.txt=./distro.info.txt --include-data-file=./version.txt=./version.txt --include-data-file=../LICENSE=./LICENSE --output-dir=%outdir% --lto=yes --follow-stdlib  --assume-yes-for-downloads --standalone ./dudecmd.py --output-filename=dudecmd
+python -m nuitka --include-data-file=./distro.info.txt=./distro.info.txt --include-data-file=./version.txt=./version.txt --include-data-file=../LICENSE=./LICENSE --output-dir=%outdir% --lto=yes --follow-stdlib  --assume-yes-for-downloads --standalone ./console.py --output-filename=dudecmd || exit /b 2
 
 move %OUTDIR%\dudecmd.dist\dudecmd.exe %OUTDIR%\dude.dist
 move %OUTDIR%\dude.dist %OUTDIR%\dude
