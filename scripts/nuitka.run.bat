3756d12eefc22d23803604d54b6548be9eea2304
piotrj <piotr.jochymek@gmail.com>
2024-01-27 09:37:04 +0100

    fix CI

--- scripts/nuitka.run.bat
+++ scripts/nuitka.run.bat
@@ -21,13 +21,13 @@ python -m nuitka --version >> distro.info.txt
 
 @echo.
 @echo running-nuitka-stage_dude
-python -m nuitka --include-data-file=./distro.info.txt=./distro.info.txt --include-data-file=./version.txt=./version.txt --include-data-file=../LICENSE=./LICENSE --enable-plugin=tk-inter --lto=yes --follow-stdlib  --assume-yes-for-downloads --windows-disable-console --output-dir=%outdir% --standalone --output-filename=dude ./dude.py || exit /b 2
+python -m nuitka --windows-icon-from-ico=./icon.ico --include-data-file=./distro.info.txt=./distro.info.txt --include-data-file=./version.txt=./version.txt --include-data-file=../LICENSE=./LICENSE --output-dir=%outdir% --standalone --lto=yes --follow-stdlib --assume-yes-for-downloads --product-version=%VERSION% --copyright="2022-2024 Piotr Jochymek" --file-description="DUplicates DEtector" --enable-plugin=tk-inter --disable-console --output-filename=dude ./dude.py || exit /b 2
 
 @echo.
 @echo running-nuitka-stage_dudecmd
-python -m nuitka --include-data-file=./distro.info.txt=./distro.info.txt --include-data-file=./version.txt=./version.txt --include-data-file=../LICENSE=./LICENSE --output-dir=%outdir% --lto=yes --follow-stdlib  --assume-yes-for-downloads --standalone ./console.py --output-filename=dudecmd || exit /b 2
+python -m nuitka --windows-icon-from-ico=./icon.ico --include-data-file=./distro.info.txt=./distro.info.txt --include-data-file=./version.txt=./version.txt --include-data-file=../LICENSE=./LICENSE --output-dir=%outdir% --standalone --lto=yes --follow-stdlib --assume-yes-for-downloads --product-version=%VERSION% --copyright="2022-2024 Piotr Jochymek" --file-description="DUplicates DEtector" ./console.py --enable-console --output-filename=dudecmd || exit /b 2
 
-move %OUTDIR%\dudecmd.dist\dudecmd.exe %OUTDIR%\dude.dist
+move %OUTDIR%\console.dist\dudecmd.exe %OUTDIR%\dude.dist
 move %OUTDIR%\dude.dist %OUTDIR%\dude
 
 @echo.
